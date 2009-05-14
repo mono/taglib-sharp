13b29dabc38a4258fbb66ca24b0ce20ed71582b3
Gabriel Burt <gburt@novell.com>
2009-05-14 20:23:56 +0000

    2009-05-14  Gabriel Burt  <gabriel.burt@gmail.com>
    	* src/TagLib/Aiff/File.cs:
    	* src/TagLib/Aiff/StreamHeader.cs:
    	* src/TagLib/FileTypes.cs:
    	* src/TagLib/TagLib.sources:
    	* src/taglib-sharp.mdp:
    	* docs/Package.en.xml.in:
    	* tests/tests.mdp:
    	* tests/fixtures/TagLib.Tests.FileFormats/AiffFormatTest.cs:
    	* tests/Makefile.am:
    	* taglib-sharp.csproj: Patch from Helmut Wahrmann adding support for the
    	AIFF file format (BGO #579261)
    
    svn path=/trunk/taglib-sharp/; revision=134161

---
+++ tests/fixtures/TagLib.Tests.FileFormats/AiffFormatTest.cs
@@ -0,0 +1,53 @@
+using System;
+using NUnit.Framework;
+using TagLib;
+
+namespace TagLib.Tests.FileFormats
+{
+	[TestFixture]
+	public class AiffFormatTest : IFormatTest
+	{
+		private static string sample_file = "samples/sample.aif";
+		private static string corrupt_file = "samples/corrupta.aif";
+		private static string tmp_file = "samples/tmpwrite.aif";
+		private File file;
+
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			file = File.Create(sample_file);
+		}
+
+		[Test]
+		public void ReadAudioProperties()
+		{
+			Assert.AreEqual(44100, file.Properties.AudioSampleRate);
+			Assert.AreEqual(2, file.Properties.Duration.Seconds);
+		}
+
+		[Test]
+		public void ReadTags()
+		{
+			Assert.AreEqual("Aiff Album", file.Tag.Album);
+			Assert.AreEqual("Aiff Artist", file.Tag.FirstPerformer);
+			Assert.AreEqual("Aiff Comment", file.Tag.Comment);
+			Assert.AreEqual("Blues", file.Tag.FirstGenre);
+			Assert.AreEqual("Aiff Title", file.Tag.Title);
+			Assert.AreEqual(5, file.Tag.Track);
+			Assert.AreEqual(10, file.Tag.TrackCount);
+			Assert.AreEqual(2009, file.Tag.Year);
+		}
+
+		[Test]
+		public void WriteStandardTags()
+		{
+			StandardTests.WriteStandardTags(sample_file, tmp_file);
+		}
+
+		[Test]
+		public void TestCorruptionResistance()
+		{
+			StandardTests.TestCorruptionResistance(corrupt_file);
+		}
+	}
+}
