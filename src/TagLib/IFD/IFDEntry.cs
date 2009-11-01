875779f9e5d34e19478d739a454699a62bab8947
Ruben Vermeersch <ruben@savanne.be>
2009-11-01 09:57:38 +0100

    Cut down on the number of warnings.

--- src/TagLib/IFD/IFDEntry.cs
+++ src/TagLib/IFD/IFDEntry.cs
@@ -24,23 +24,6 @@
 
 namespace TagLib.IFD
 {
-	public enum IFDEntryType : ushort
-	{
-		Unknown = 0,
-		Byte = 1,
-		Ascii = 2,
-		Short = 3,
-		Long = 4,
-		Rational = 5,
-		SByte = 6,
-		Undefined = 7,
-		SShort = 8,
-		SLong = 9,
-		SRational = 10,
-		Float = 11,
-		Double = 12
-	}
-
 	/// <summary>
 	///    An IFD entry, which is a key/value pair inside an IFD.
 	/// </summary>
