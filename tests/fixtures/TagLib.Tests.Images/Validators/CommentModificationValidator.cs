d11b9bfe861b47e0bbfd3a999376bdeb265cb802
Mike Gemünde <mike@gemuende.de>
2010-05-27 15:46:29 +0200

    Add more flexible PropertyModificationValidator
    The new class PropertyModificationValidator allows to test modifications for
    every property defined in ImageTag. This makes it more flexible to test them
    all.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=619920

--- tests/fixtures/TagLib.Tests.Images/Validators/CommentModificationValidator.cs
+++ tests/fixtures/TagLib.Tests.Images/Validators/CommentModificationValidator.cs
@@ -7,51 +7,16 @@ namespace TagLib.Tests.Images.Validators
 	///    This class tests the modification of the Comment field,
 	///    regardless of which metadata format is used.
 	/// </summary>
-	public class CommentModificationValidator : IMetadataModificationValidator
+	public class CommentModificationValidator : PropertyModificationValidator<string>
 	{
-		string orig_comment;
-		string test_comment = "This is a TagLib# &Test?Comment%$@_ ";
-
 		public CommentModificationValidator () : this (String.Empty) { }
 
 		public CommentModificationValidator (string orig_comment)
-		{
-			this.orig_comment = orig_comment;
-		}
+			: this (orig_comment, "This is a TagLib# &Test?Comment%$@_ ")
+		{}
 
 		public CommentModificationValidator (string orig_comment, string test_comment)
-			: this (orig_comment)
-		{
-			this.test_comment = test_comment;
-		}
-
-		/// <summary>
-		///    Check if the original comment is found.
-		/// </summary>
-		public virtual void ValidatePreModification (Image.File file) {
-			Assert.AreEqual (orig_comment, GetTag (file).Comment);
-		}
-
-		/// <summary>
-		///    Changes the comment.
-		/// </summary>
-		public virtual void ModifyMetadata (Image.File file) {
-			GetTag (file).Comment = test_comment;
-		}
-
-		/// <summary>
-		///    Validates if changes survived a write.
-		/// </summary>
-		public void ValidatePostModification (Image.File file) {
-			Assert.AreEqual (test_comment, GetTag (file).Comment);
-		}
-
-		/// <summary>
-		///    Returns the tag that should be tested. Default
-		///    behavior is no specific tag.
-		/// </summary>
-		public virtual Image.ImageTag GetTag (Image.File file) {
-			return file.ImageTag;
-		}
+			: base ("Comment", orig_comment, test_comment)
+		{}
 	}
 }
--- tests/fixtures/TagLib.Tests.Images/Validators/TagCommentModificationValidator.cs
+++ tests/fixtures/TagLib.Tests.Images/Validators/TagCommentModificationValidator.cs
@@ -7,49 +7,18 @@ namespace TagLib.Tests.Images.Validators
 	///    This class tests the modification of the Comment field,
 	///    in a specific tag.
 	/// </summary>
-	public class TagCommentModificationValidator : CommentModificationValidator
+	public class TagCommentModificationValidator : TagPropertyModificationValidator<string>
 	{
-		bool tag_present;
-		TagTypes type;
+		public TagCommentModificationValidator (TagTypes type, bool tag_present)
+			: this (String.Empty, type, tag_present)
+		{}
 
-		public TagCommentModificationValidator (TagTypes type, bool tag_present) : this (String.Empty, type, tag_present) { }
-
-		public TagCommentModificationValidator (string orig_comment, TagTypes type, bool tag_present) : base (orig_comment)
-		{
-			this.type = type;
-			this.tag_present = tag_present;
-		}
+		public TagCommentModificationValidator (string orig_comment, TagTypes type, bool tag_present)
+			: this (orig_comment, "This is a TagLib# &Test?Comment%$@_ ", type, tag_present)
+		{}
 
 		public TagCommentModificationValidator (string orig_comment, string test_comment, TagTypes type, bool tag_present)
-			: base (orig_comment, test_comment)
-		{
-			this.type = type;
-			this.tag_present = tag_present;
-		}
-
-		/// <summary>
-		///    Check if the original comment is found.
-		/// </summary>
-		public override void ValidatePreModification (Image.File file) {
-			if (!tag_present) {
-				Assert.IsNull (file.GetTag (type, false));
-			} else {
-				Assert.IsNotNull (file.GetTag (type, false));
-				base.ValidatePreModification (file);
-			}
-		}
-
-		/// <summary>
-		///    Creates the tag if needed.
-		/// </summary>
-		public override void ModifyMetadata (Image.File file) {
-			if (!tag_present)
-				file.GetTag (type, true);
-			base.ModifyMetadata (file);
-		}
-
-		public override Image.ImageTag GetTag (Image.File file) {
-			return file.GetTag (type, false) as Image.ImageTag;
-		}
+			: base ("Comment", orig_comment, test_comment, type, tag_present)
+		{}
 	}
 }
