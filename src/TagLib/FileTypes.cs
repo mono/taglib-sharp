b664ec978484b78805e71a88c6fdf8d589564039
Mike Gemünde <mike@gemuende.de>
2010-06-14 10:13:22 +0200

    Add support for Canon CR2 raw files
    This commits adds support to parse Canon CR2 RAW files. Since this
    format is based on the TIFF structure, reading is mostly based on
    this code.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=621508

--- src/TagLib/FileTypes.cs
+++ src/TagLib/FileTypes.cs
@@ -62,6 +62,7 @@ namespace TagLib {
 			typeof(TagLib.Aiff.File),
 			typeof(TagLib.Ape.File),
 			typeof(TagLib.Asf.File),
+			typeof(TagLib.Cr2.File),
 			typeof(TagLib.Flac.File),
 			typeof(TagLib.Gif.File),
 			typeof(TagLib.MusePack.File),
