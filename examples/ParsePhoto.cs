3a625b1054f3cb1eed898471dbb4406783d94142
Ruben Vermeersch <ruben@savanne.be>
2010-06-27 17:11:51 +0200

    Indent values in ParsePhoto.

--- examples/ParsePhoto.cs
+++ examples/ParsePhoto.cs
@@ -40,31 +40,31 @@ public class ParsePhotoApp
 		Console.WriteLine (path);
 		Console.WriteLine (String.Empty);
 
-		Console.WriteLine("Tags in object:  " +  image.TagTypes);
+		Console.WriteLine("Tags in object  : " +  image.TagTypes);
 		Console.WriteLine (String.Empty);
 
-		Console.WriteLine("Comment:         " +  image.ImageTag.Comment);
-		Console.Write("Keywords:        ");
+		Console.WriteLine("Comment         : " +  image.ImageTag.Comment);
+		Console.Write("Keywords        : ");
 		foreach (var keyword in image.ImageTag.Keywords) {
 			Console.Write (keyword + " ");
 		}
 		Console.WriteLine ();
-		Console.WriteLine("Rating:          " +  image.ImageTag.Rating);
-		Console.WriteLine("DateTime:        " +  image.ImageTag.DateTime);
-		Console.WriteLine("Orientation:     " +  image.ImageTag.Orientation);
-		Console.WriteLine("Software:        " +  image.ImageTag.Software);
-		Console.WriteLine("ExposureTime:    " +  image.ImageTag.ExposureTime);
-		Console.WriteLine("FNumber:         " +  image.ImageTag.FNumber);
-		Console.WriteLine("ISOSpeedRatings: " +  image.ImageTag.ISOSpeedRatings);
-		Console.WriteLine("FocalLength:     " +  image.ImageTag.FocalLength);
-		Console.WriteLine("FocalLength35mm: " +  image.ImageTag.FocalLengthIn35mmFilm);
-		Console.WriteLine("Make:            " +  image.ImageTag.Make);
-		Console.WriteLine("Model:           " +  image.ImageTag.Model);
+		Console.WriteLine("Rating          : " +  image.ImageTag.Rating);
+		Console.WriteLine("DateTime        : " +  image.ImageTag.DateTime);
+		Console.WriteLine("Orientation     : " +  image.ImageTag.Orientation);
+		Console.WriteLine("Software        : " +  image.ImageTag.Software);
+		Console.WriteLine("ExposureTime    : " +  image.ImageTag.ExposureTime);
+		Console.WriteLine("FNumber         : " +  image.ImageTag.FNumber);
+		Console.WriteLine("ISOSpeedRatings : " +  image.ImageTag.ISOSpeedRatings);
+		Console.WriteLine("FocalLength     : " +  image.ImageTag.FocalLength);
+		Console.WriteLine("FocalLength35mm : " +  image.ImageTag.FocalLengthIn35mmFilm);
+		Console.WriteLine("Make            : " +  image.ImageTag.Make);
+		Console.WriteLine("Model           : " +  image.ImageTag.Model);
 
 		if (image.Properties != null) {
-			Console.WriteLine("Width   :        " +  image.Properties.PhotoWidth);
-			Console.WriteLine("Height  :        " +  image.Properties.PhotoHeight);
-			Console.WriteLine("Type    :        " +  image.Properties.Description);
+			Console.WriteLine("Width           : " +  image.Properties.PhotoWidth);
+			Console.WriteLine("Height          : " +  image.Properties.PhotoHeight);
+			Console.WriteLine("Type            : " +  image.Properties.Description);
 		}
 
 		Console.WriteLine (String.Empty);
