e4380983772a56b81c5527404325f6fd7efd778d
Tim Howard <timothy.howard@gmail.com>
2012-06-30 16:16:35 -0400

    Mark the file as corrupt instead of throwing exception
    Fixes bgo#643477
    
    Signed-off-by: Alexander Kojevnikov <alexander@kojevnikov.com>

--- src/TagLib/IFD/IFDReader.cs
+++ src/TagLib/IFD/IFDReader.cs
@@ -803,8 +803,10 @@ namespace TagLib.IFD
 				length = 1073741824L * 4;
 			}
 
-			if (makernote_offset > length)
-			    throw new Exception ("offset to makernote is beyond file size");
+			if (makernote_offset > length) {
+				file.MarkAsCorrupt ("offset to makernote is beyond file size");
+				return null;
+			}
 
 			if (makernote_offset + header_size > length)
 				throw new Exception ("data is to short to contain a maker note ifd");
