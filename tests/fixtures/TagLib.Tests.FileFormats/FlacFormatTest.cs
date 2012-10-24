e892e98d9ee002b3e645dc96c3b7344a0fdc534a
Ivailo Karamanolev <ivailo@karamanolev.com>
2012-10-24 00:16:03 -0700

    Add tests for the ReplayGain tags for mp3 and flac including sample files.

--- tests/fixtures/TagLib.Tests.FileFormats/FlacFormatTest.cs
+++ tests/fixtures/TagLib.Tests.FileFormats/FlacFormatTest.cs
@@ -57,5 +57,50 @@ namespace TagLib.Tests.FileFormats
         {
             StandardTests.TestCorruptionResistance ("samples/corrupt/a.flac");
         }
+
+        [Test]
+        public void ReplayGainTest()
+        {
+            string inFile = "samples/sample_replaygain.flac";
+            string tempFile = "samples/tmpwrite_sample_replaygain.flac";
+
+            File rgFile = File.Create(inFile);
+            Assert.AreEqual(1.8d, rgFile.Tag.ReplayGainTrackGain);
+            Assert.AreEqual(0.462341d, rgFile.Tag.ReplayGainTrackPeak);
+            Assert.AreEqual(2.8d, rgFile.Tag.ReplayGainAlbumGain);
+            Assert.AreEqual(0.562341d, rgFile.Tag.ReplayGainAlbumPeak);
+            rgFile.Dispose();
+
+            System.IO.File.Copy(inFile, tempFile, true);
+
+            rgFile = File.Create(tempFile);
+            rgFile.Tag.ReplayGainTrackGain = -1;
+            rgFile.Tag.ReplayGainTrackPeak = 1;
+            rgFile.Tag.ReplayGainAlbumGain = 2;
+            rgFile.Tag.ReplayGainAlbumPeak = 0;
+            rgFile.Save();
+            rgFile.Dispose();
+
+            rgFile = File.Create(tempFile);
+            Assert.AreEqual(-1d, rgFile.Tag.ReplayGainTrackGain);
+            Assert.AreEqual(1d, rgFile.Tag.ReplayGainTrackPeak);
+            Assert.AreEqual(2d, rgFile.Tag.ReplayGainAlbumGain);
+            Assert.AreEqual(0d, rgFile.Tag.ReplayGainAlbumPeak);
+            rgFile.Tag.ReplayGainTrackGain = double.NaN;
+            rgFile.Tag.ReplayGainTrackPeak = double.NaN;
+            rgFile.Tag.ReplayGainAlbumGain = double.NaN;
+            rgFile.Tag.ReplayGainAlbumPeak = double.NaN;
+            rgFile.Save();
+            rgFile.Dispose();
+            
+            rgFile = File.Create(tempFile);
+            Assert.AreEqual(double.NaN, rgFile.Tag.ReplayGainTrackGain);
+            Assert.AreEqual(double.NaN, rgFile.Tag.ReplayGainTrackPeak);
+            Assert.AreEqual(double.NaN, rgFile.Tag.ReplayGainAlbumGain);
+            Assert.AreEqual(double.NaN, rgFile.Tag.ReplayGainAlbumPeak);
+            rgFile.Dispose();
+
+            System.IO.File.Delete(tempFile);
+        }
     }
 }
