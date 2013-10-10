2006fea42300fe79302f101d3cf82b1a74968ab4
Stephen Shaw <sshaw@decriptor.com>
2013-10-10 05:20:14 -0600

    Include the AssemblyInfo.cs file
    This is a bit redundant because its generated by
    autotools, but this will allow others to include
    taglib-sharp.csproj in their solution and build
    without having to run autotools

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+//
+// AssemblyInfo.cs.in: Contains flags to use for the assembly.
+//
+// Author:
+//   Brian Nickel (brian.nickel@gmail.com)
+//
+// Copyright (C) 2006-2007 Brian Nickel
+// 
+// This library is free software; you can redistribute it and/or modify
+// it  under the terms of the GNU Lesser General Public License version
+// 2.1 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+// USA
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly:AssemblyVersion("2.1.0.0")]
+[assembly:AssemblyTitle ("TagLib#")]
+[assembly:AssemblyDescription ("A library for reading and writing audio metatags.")]
+[assembly:AssemblyCopyright ("Copyright (c) 2006-2007 Brian Nickel.  Copyright (c) 2009-2010 Other contributors")]
+[assembly:AssemblyCompany ("")]
+[assembly:AssemblyDelaySign(false)]
+[assembly:AssemblyKeyFile("taglib-sharp.snk")]
+[assembly:CLSCompliant(false)]
