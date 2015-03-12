363bbcf54833177071e0296e0e94326a027afea6
hwahrmann <hwahrmann@team-mediaportal.com>
2015-03-12 13:30:14 +0100

    Implemented support for DSF files
    Added Unit Tests for Dsf Format
    
    Changed project files to include the DSF specific tests
    
    Test DSF Sample Rate
    
    DSF Test was done for wrong audio properties

---
+++ tests/fixtures/TagLib.Tests.FileFormats/DsfFormatTest.cs
@@ -0,0 +1,50 @@
+using System;
+using NUnit.Framework;
+using TagLib;
+
+namespace TagLib.Tests.FileFormats
+{
+	[TestFixture]
+	public class DsfFormatTest : IFormatTest
+	{
+		private static string sample_file = "samples/sample.dsf";
+		private static string tmp_file = "samples/tmpwrite.dsf";
+		private File file;
+
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			file = File.Create(sample_file);
+		}
+
+		[Test]
+		public void ReadAudioProperties()
+		{
+			Assert.AreEqual(2822400, file.Properties.AudioSampleRate);
+		}
+
+		[Test]
+		public void ReadTags()
+		{
+			Assert.AreEqual("Dsf Album", file.Tag.Album);
+			Assert.AreEqual("Dsf Artist", file.Tag.FirstPerformer);
+			Assert.AreEqual("Dsf Comment", file.Tag.Comment);
+			Assert.AreEqual("Rock", file.Tag.FirstGenre);
+			Assert.AreEqual("Dsf Title", file.Tag.Title);
+			Assert.AreEqual(1, file.Tag.Track);
+			Assert.AreEqual(2016, file.Tag.Year);
+		}
+
+		[Test]
+		public void WriteStandardTags()
+		{
+			StandardTests.WriteStandardTags(sample_file, tmp_file);
+		}
+
+		[Test]
+		public void TestCorruptionResistance()
+		{
+			StandardTests.TestCorruptionResistance("samples/corrupt/a.dsf");
+		}
+	}
+}
