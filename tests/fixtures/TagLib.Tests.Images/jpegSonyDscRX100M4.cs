ea3b52f52c05e999e328aeb9375dfe33775150ce
David-Tillmann Schaefer <codingdave@gmail.com>
2016-09-05 18:07:54 +0200

    Fix for OverflowException (Issue System.OverflowException in IFDReader.CreateIDFEntry)

--- tests/fixtures/TagLib.Tests.Images/jpegSonyDscRX100M4.cs
+++ tests/fixtures/TagLib.Tests.Images/jpegSonyDscRX100M4.cs
@@ -33,7 +33,7 @@ namespace TagLib.Tests.Images
 						bool isSuccess = true;
 						try
 						{
-						File.Create ("samples/sample_sony_DSC-RX100M4.jpg");
+							File.Create ("samples/sample_sony_DSC-RX100M4.jpg");
 						}
 						catch(ArithmeticException e) {
 								// Old versions of TagLib were throwing an ArithmeticException on reading in Sony DSC RX100M4 images
