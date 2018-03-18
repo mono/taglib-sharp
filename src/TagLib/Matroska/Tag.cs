2bc04f1064a152ace894a453c9006ef4ff3b6ca4
zductiv <zductiv@gmail.com>
2018-03-18 10:45:46 +0800

    testing vs spaces v tabs

--- src/TagLib/Matroska/Tag.cs
+++ src/TagLib/Matroska/Tag.cs
@@ -1233,31 +1233,31 @@ namespace TagLib.Matroska
 			set { }
 		}
 
-        /// <summary>
-        ///    Gets and sets the MusicBrainz Release Group ID of
-        ///    the media described by the current instance.
-        /// </summary>
-        /// <value>
-        ///    A <see cref="string" /> containing the MusicBrainz 
-        ///    ReleaseGroupID for the media described by the current
-        ///    instance or null if no value is present.
-        /// </value>
-        public override string MusicBrainzReleaseGroupId
-        {
-            get { return null; }
-            set { }
-        }
+		/// <summary>
+		///    Gets and sets the MusicBrainz Release Group ID of
+		///    the media described by the current instance.
+		/// </summary>
+		/// <value>
+		///    A <see cref="string" /> containing the MusicBrainz 
+		///    ReleaseGroupID for the media described by the current
+		///    instance or null if no value is present.
+		/// </value>
+		public override string MusicBrainzReleaseGroupId
+		{
+			get { return null; }
+			set { }
+		}
 
-        /// <summary>
-        ///    Gets and sets the MusicBrainz Release ID of
-        ///    the media described by the current instance.
-        /// </summary>
-        /// <value>
-        ///    A <see cref="string" /> containing the MusicBrainz 
-        ///    ReleaseID for the media described by the current
-        ///    instance or null if no value is present.
-        /// </value>
-        public override string MusicBrainzReleaseId
+		/// <summary>
+		///    Gets and sets the MusicBrainz Release ID of
+		///    the media described by the current instance.
+		/// </summary>
+		/// <value>
+		///    A <see cref="string" /> containing the MusicBrainz 
+		///    ReleaseID for the media described by the current
+		///    instance or null if no value is present.
+		/// </value>
+		public override string MusicBrainzReleaseId
 		{
 			get { return null; }
 			set { }
