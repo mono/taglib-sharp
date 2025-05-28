be8122ad56ec6e583517dafab959426743701c28
Stephen Shaw <sshaw@decriptor.com>
2025-05-27 22:22:10 -0600

    Touch up BatchSet program

--- examples/BatchSet/Program.cs
+++ examples/BatchSet/Program.cs
@@ -2,25 +2,29 @@ using System;
 using System.Collections.Generic;
 using TagLib;
 
-public class BatchSet
+namespace BatchSet;
+
+public class Program
 {
-	private enum Mode {
-		Tag, Value, File
+	enum Mode {
+		Tag,
+		Value,
+		File
 	}
-	
+
 	public static void Main(string [] args)
 	{
 		if(args.Length < 3) {
 			Console.Error.WriteLine ("USAGE: BatchSet.exe -tag value [-tag2 value ...] File1 [File2 ...]");
 			return;
 		}
-		
+
 		Mode mode = Mode.Tag;
-		List<string> files = new List<string> ();
-		Dictionary<string,string> tags  = new Dictionary<string,string> ();
-		
+		var files = new List<string> ();
+		var tags  = new Dictionary<string,string> ();
+
 		string tag = null;
-		
+
 		foreach (string str in args) {
 			if (mode == Mode.Tag) {
 				if (str [0] == '-') {
@@ -30,30 +34,30 @@ public class BatchSet
 						tag = str.Substring (1);
 						mode = Mode.Value;
 					}
-					
+
 					continue;
 				}
 				mode = Mode.File;
 			}
-			
+
 			if (mode == Mode.Value) {
 				if (!tags.ContainsKey (tag))
 					tags.Add (tag, str);
 				mode = Mode.Tag;
 				continue;
 			}
-			
+
 			if (mode == Mode.File)
 				files.Add (str);
 		}
-		
+
 		foreach (string filename in files) {
-			TagLib.File file = TagLib.File.Create (filename);
+			using var file = TagLib.File.Create (filename);
 			if (file == null)
 				continue;
-			
-			Console.WriteLine ("Updating Tags For: " + filename);
-		
+
+			Console.WriteLine ($"Updating Tags For: {filename}");
+
 			foreach (string key in tags.Keys) {
 				string value = tags [key];
 				try {
@@ -63,11 +67,7 @@ public class BatchSet
 						if (number == 1) {
 							file.RemoveTags (TagTypes.Id3v2);
 						} else {
-							TagLib.Id3v2.Tag v2 =
-								file.GetTag (TagTypes.Id3v2, true)
-								as TagLib.Id3v2.Tag;
-							
-							if (v2 != null)
+							if (file.GetTag (TagTypes.Id3v2, true) is TagLib.Id3v2.Tag v2)
 								v2.Version = number;
 						}
 						break;
@@ -75,7 +75,7 @@ public class BatchSet
 						file.Tag.Album = value;
 						break;
 					case "artists":
-						file.Tag.AlbumArtists = value.Split (new char [] {';'});
+						file.Tag.AlbumArtists = value.Split ([';']);
 						break;
 					case "comment":
 						file.Tag.Comment = value;
@@ -84,7 +84,7 @@ public class BatchSet
 						file.Tag.Lyrics = value;
 						break;
 					case "composers":
-						file.Tag.Composers = value.Split (new char [] {';'});
+						file.Tag.Composers = value.Split ([';']);
 						break;
 					case "disc":
 						file.Tag.Disc = uint.Parse (value);
@@ -93,10 +93,10 @@ public class BatchSet
 						file.Tag.DiscCount = uint.Parse (value);
 						break;
 					case "genres":
-						file.Tag.Genres = value.Split (new char [] {';'});
+						file.Tag.Genres = value.Split ([';']);
 						break;
 					case "performers":
-						file.Tag.Performers = value.Split (new char [] {';'});
+						file.Tag.Performers = value.Split ([';']);
 						break;
 					case "title":
 						file.Tag.Title = value;
@@ -111,20 +111,20 @@ public class BatchSet
 						file.Tag.Year = uint.Parse (value);
 						break;
 					case "pictures":
-						List<Picture> pics = new List<Picture> ();
+						var pics = new List<Picture> ();
 						if (!string.IsNullOrEmpty (value))
-							foreach (string path in value.Split (new char [] {';'})) {
+							foreach (string path in value.Split ([';'])) {
 								pics.Add (new Picture (path));
 							}
 						file.Tag.Pictures = pics.ToArray ();
 						break;
 					}
 				} catch (Exception e) {
-					Console.WriteLine ("Error setting tag " + key + ":");
+					Console.WriteLine ($"Error setting tag {key}:");
 					Console.WriteLine (e);
 				}
 			}
-			
+
 			file.Save();
 		}
 	}
